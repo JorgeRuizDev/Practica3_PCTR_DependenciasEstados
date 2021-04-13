5485b5ebe6cb63ed5fa3f607627a33e0ce4a66e0
Jorge <jrg1011@alu.ubu.es>
2021-04-13 17:38:55 +0200

    Añadido JavaDoc
    Signed-off-by: Jorge <jrg1011@alu.ubu.es>

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -4,9 +4,19 @@ import java.util.Hashtable;
 
 public class Parque implements IParque {
 
+	/**
+	 * Variable que controla el máximo de personas en el parque.
+	 */
 	public static int MAX_PERSONAS = 50;
 
+	/**
+	 * Variable que almacena el número de personas que hay dentro del parque.
+	 */
 	private int contadorPersonasTotales;
+
+	/**
+	 * Mapa que controla el balance de Entrada/Salida de cada puerta
+	 */
 	private final Hashtable<String, Integer> contadoresPersonasPuerta;
 
 	/**
@@ -17,30 +27,41 @@ public class Parque implements IParque {
 		contadoresPersonasPuerta = new Hashtable<>();
 	}
 
-    @Override
+	/**
+	 * Método sincronizado que pemite a un hilo introducir a una persona al parque
+	 * por una puerta (identificada por su ID).
+	 *
+	 * @param puerta Un String  con el id de la puerta.
+	 */
+	@Override
 	public synchronized void entrarAlParque(String puerta) {
 
-        comprobarAntesDeEntrar(); // Wait
+		comprobarAntesDeEntrar(); // Wait
 
 		// Aumentamos el contador total y el individual
-        incrementarContador(puerta);
+		incrementarContador(puerta);
 
-        // Comprobamos invariante
+		// Comprobamos invariante
 		checkInvariante();
 
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Entrada");
 
-        this.notifyAll();
+		this.notifyAll();
 	}
 
+	/**
+	 * Método de salida del parque. Disminuye en 1 la cantidad de personas en el parque.
+	 *
+	 * @param puerta Un String  con el id de la puerta.
+	 */
 	@Override
 	public synchronized void salirDelParque(String puerta) {
 
-        comprobarAntesDeSalir();
+		comprobarAntesDeSalir();
 
 		// Disminuimos el contador total y el individual
-        decrementarContador(puerta);
+		decrementarContador(puerta);
 
 		// Comprobamos invariante
 		checkInvariante();
@@ -48,9 +69,12 @@ public class Parque implements IParque {
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Salida");
 
-        this.notifyAll();
+		this.notifyAll();
 	}
 
+	/**
+	 * Método que saca por pantalla el estado del parque.
+	 */
 	private void imprimirInfo(String puerta, String movimiento) {
 		System.out.println(movimiento + " por puerta " + puerta);
 		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
@@ -62,50 +86,82 @@ public class Parque implements IParque {
 		System.out.println(" ");
 	}
 
+	/**
+	 * Método auxiliar que permite obtener el número total de personas en el parque según el hashmap.
+	 *
+	 * @return el número de personas que hay acutalmente en el parque.
+	 */
 	private int sumarContadoresPuerta() {
 		return contadoresPersonasPuerta.values().stream().mapToInt(Integer::intValue).sum();
 	}
 
+	/**
+	 * Método que permite actualizar el hashmap
+	 *
+	 * @param puerta id de la puerta.
+	 * @param number nuevo valor a asignar en la puerta (+1 o -1)
+	 */
+	private void changeVal(String puerta, int number) {
+		int val = 0;
+		if (this.contadoresPersonasPuerta.containsKey(puerta))
+			val = this.contadoresPersonasPuerta.get(puerta);
+		this.contadoresPersonasPuerta.put(puerta, val + number);
+	}
 
-    private void changeVal(String puerta, int number) {
-        int val = 0;
-        if (this.contadoresPersonasPuerta.containsKey(puerta))
-            val = this.contadoresPersonasPuerta.get(puerta);
-        this.contadoresPersonasPuerta.put(puerta, val + number);
-    }
+	/**
+	 * Método que incremental el número de personas en el parque.
+	 *
+	 * @param puerta id Puerta
+	 */
+	private void incrementarContador(String puerta) {
+		this.changeVal(puerta, 1);
+		this.contadorPersonasTotales++;
+	}
 
-    private void incrementarContador(String puerta) {
-        this.changeVal(puerta, 1);
-        this.contadorPersonasTotales++;
-    }
+	/**
+	 * Método que incremental el número de personas en el parque.
+	 *
+	 * @param puerta id Puerta
+	 */
+	private void decrementarContador(String puerta) {
+		this.changeVal(puerta, -1);
+		this.contadorPersonasTotales--;
+	}
 
-    private void decrementarContador(String puerta) {
-        this.changeVal(puerta, -1);
-        this.contadorPersonasTotales--;
-    }
+	/**
+	 * Método que deja a un hilo en espera.
+	 */
+	private void waitForCheck() {
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void waitForCheck() {
-        try {
-            this.wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
+	/**
+	 * Comprobación de invariantes que controlen la exclusión mutua.
+	 */
 	protected void checkInvariante() {
 		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parque";
 		assert contadorPersonasTotales <= MAX_PERSONAS : "INV: El parque tiene más personas que su capacidad";
 		assert contadorPersonasTotales >= 0 : "INV: El parque tiene personas demasiado negativas";
 	}
 
+	/**
+	 * Método que realiza comprobaciones antes de introducir a un usuario en el parque.
+	 */
 	protected void comprobarAntesDeEntrar() {
 		while (contadorPersonasTotales > MAX_PERSONAS)
-            waitForCheck();
+			waitForCheck();
 	}
 
+	/**
+	 * Método que realiza comprobaciones antes de expulsar a un usuario del parque.
+	 */
 	protected void comprobarAntesDeSalir() {
 		while (contadorPersonasTotales < 1)
-            waitForCheck();
+			waitForCheck();
 	}
 
 
