a3606f75a0d6558444ae72fc279b822e5886c924
Jorge <jrg1011@alu.ubu.es>
2021-04-13 10:13:48 +0200

    Actualizada la salida

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -5,72 +5,81 @@ import java.util.Hashtable;
 public class Parque implements IParque {
 
 	public static int MAX_PERSONAS = 50;
+	private int contadorPersonasTotales;
+	private final Hashtable<String, Integer> contadoresPersonasPuerta;
 
-  private int contadorPersonasTotales;
-  private final Hashtable<String, Integer> contadoresPersonasPuerta;
+	public Parque() {
+		contadorPersonasTotales = 0;
+		contadoresPersonasPuerta = new Hashtable<String, Integer>();
+	}
 
-  public Parque() {
-    contadorPersonasTotales = 0;
-    contadoresPersonasPuerta = new Hashtable<String, Integer>();
-  }
+	@Override
+	public void entrarAlParque(String puerta) {
 
-  @Override
-  public void entrarAlParque(String puerta) {
+		// Si no hay entradas por esa puerta, inicializamos
+		contadoresPersonasPuerta.putIfAbsent(puerta, 0);
 
-    // Si no hay entradas por esa puerta, inicializamos
-    contadoresPersonasPuerta.putIfAbsent(puerta, 0);
+		// Aumentamos el contador total y el individual
+		contadorPersonasTotales++;
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) + 1);
 
-    // Aumentamos el contador total y el individual
-    contadorPersonasTotales++;
-    contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) + 1);
+		// Comprobamos invariante
+		checkInvariante();
 
-    // Comprobamos invariante
-    checkInvariante();
+		// Imprimimos el estado del parque
+		imprimirInfo(puerta, "Entrada");
+	}
 
-    // Imprimimos el estado del parque
-    imprimirInfo(puerta, "Entrada");
-  }
+	@Override
+	public synchronized void salirDelParque(String puerta) {
 
-  @Override
-  public void salirDelParque(String puerta) {
-    // Disminuimos el contador total y el individual
-    contadorPersonasTotales--;
-    contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) - 1);
+		comprobarAntesDeSalir();
 
-    // Comprobamos invariante
-    checkInvariante();
+		// Disminuimos el contador total y el individual
+		contadorPersonasTotales--;
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) - 1);
 
-    // Imprimimos el estado del parque
-    imprimirInfo(puerta, "Salida");
-  }
+		// Comprobamos invariante
+		checkInvariante();
 
-  private void imprimirInfo(String puerta, String movimiento) {
-    System.out.println(movimiento + " por puerta " + puerta);
-    System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
+		// Imprimimos el estado del parque
+		imprimirInfo(puerta, "Salida");
+		notify();
+	}
 
-    // Iteramos por todas las puertas e imprimimos sus entradas
-    for (String p : contadoresPersonasPuerta.keySet())
-      System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
+	private void imprimirInfo(String puerta, String movimiento) {
+		System.out.println(movimiento + " por puerta " + puerta);
+		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
 
-    System.out.println(" ");
-  }
+		// Iteramos por todas las puertas e imprimimos sus entradas
+		for (String p : contadoresPersonasPuerta.keySet())
+			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
 
-  private int sumarContadoresPuerta() {
-    return contadoresPersonasPuerta.values().stream().mapToInt(Integer::intValue).sum();
-  }
+		System.out.println(" ");
+	}
 
-  protected void checkInvariante() {
-    assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
-  }
+	private int sumarContadoresPuerta() {
+		return contadoresPersonasPuerta.values().stream().mapToInt(Integer::intValue).sum();
+	}
 
-  protected void comprobarAntesDeEntrar() {  // TODO
-    boolean entradaDisp = sumarContadoresPuerta() < MAX_PERSONAS;
+	protected void checkInvariante() {
+		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
+	}
 
-  }
+	protected void comprobarAntesDeEntrar() {  // TODO
+		boolean entradaDisp = sumarContadoresPuerta() < MAX_PERSONAS;
 
-  protected void comprobarAntesDeSalir() {    // TODO
-    boolean hay = sumarContadoresPuerta() > 0;
-  }
+	}
+
+	protected void comprobarAntesDeSalir() {
+		if (contadorPersonasTotales < 1) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 
 }
