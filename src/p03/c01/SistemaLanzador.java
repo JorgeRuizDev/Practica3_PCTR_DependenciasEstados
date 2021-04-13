230ec59a487dd0f0697ce87672838bfd4c3bd54f
Jorge <jrg1011@alu.ubu.es>
2021-04-13 09:01:22 +0200

    Añadido Documentación SIstema Lanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,6 +1,19 @@
 package src.p03.c01;
 
+/**
+ *
+ * Clase Sistema Lanzador
+ * Cotiene el Main del proyecto que lanza los diferentes hilos.
+ *
+ * @author Iván Ruiz Gázquez <a>irg1008@alu.ubu.es</a>
+ * @author Jorge Ruiz Gómez <a>jrg1011@alu.ubu.es</a>
+ */
 public class SistemaLanzador {
+
+	/**
+	 * Main del proyecto.
+	 * @param args args[0]: Número de puertas a crear.
+	 */
 	public static void main(String[] args) {
 		
 		IParque parque = new Parque(); // TODO
