15b546ad680f96a6d86672b8302db67504e40b6d
Jorge <jrg1011@alu.ubu.es>
2021-04-13 09:53:24 +0200

    Actualizado el Lanzador / Main

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,39 +1,39 @@
 package src.p03.c01;
 
 /**
- *
  * Clase Sistema Lanzador
  * Cotiene el Main del proyecto que lanza los diferentes hilos.
  *
  * @author Iván Ruiz Gázquez <a>irg1008@alu.ubu.es</a>
  * @author Jorge Ruiz Gómez <a>jrg1011@alu.ubu.es</a>
+ * @author José Barbero <a>https://github.com/JoseBarbero</a>
  */
 public class SistemaLanzador {
 
 	/**
 	 * Main del proyecto.
+	 *
 	 * @param args args[0]: Número de puertas a crear.
 	 */
 	public static void main(String[] args) {
-		
+
 		IParque parque = new Parque(); // TODO
 		char letra_puerta = 'A';
-		
+
 		System.out.println("¡Parque abierto!");
-		
+
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
-			
+
+			String puerta = "" + letra_puerta++;
+
 			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
-			new Thread (entradas).start();
-			
-			// 
-			// TODO
-			//
-			
-			
+			new Thread(entradas).start();
+
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread(salidas).start();
+
+
 		}
-	}	
+	}
 }
